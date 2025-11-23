7efd3559e90ec3d843000959c3823ed94d344a24
Shady <vimalan.github@gmail.com>
2025-11-23 15:54:50 +0530

    Guessing game

--- guess/src/main.rs
+++ guess/src/main.rs
@@ -1,8 +1,18 @@
 use std::io;
-fn main() {
-    println!("Hello, world!");
-    println!("Enter the guess:");
+use rand::Rng;
+use std::cmp::Ordering;
+fn main(){
+    let sec=rand::thread_rng().gen_range(1,101);
     let mut guess=String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to readline");
-    println!("You guess:{guess}");
+    io::stdin().read_line(&mut guess).expect("Failed to get input");
+    let guess: u32 = match guess.trim().parse(){
+        Ok(num)=>num,
+        Err(_)=>{return;}
+    };
+    match guess.cmp(&sec){
+        Ordering::Less=>println!("Smaller"),
+        Ordering::Greater=>println!("Larger"),
+        Ordering::Equal=>println!("You Win!"),
+    };
+
 }
