e7d4c6e1613c5ce2dfceafce5bd76cd0788b9dda
Shady <vimalan.github@gmail.com>
2025-11-23 16:03:45 +0530

    Guess colored

--- guess/src/main.rs
+++ guess/src/main.rs
@@ -1,8 +1,11 @@
 use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use colored::*;
 fn main(){
     let sec=rand::thread_rng().gen_range(1,101);
+    loop{
+    println!("Enter you Guess: ");
     let mut guess=String::new();
     io::stdin().read_line(&mut guess).expect("Failed to get input");
     let guess: u32 = match guess.trim().parse(){
@@ -10,9 +13,10 @@ fn main(){
         Err(_)=>{return;}
     };
     match guess.cmp(&sec){
-        Ordering::Less=>println!("Smaller"),
-        Ordering::Greater=>println!("Larger"),
-        Ordering::Equal=>println!("You Win!"),
+        Ordering::Less=>println!("{}","Smaller".red()),
+        Ordering::Greater=>println!("{}","Larger".red()),
+        Ordering::Equal=>{println!("{}","You Win!".green());break;},
     };
+}
 
 }
